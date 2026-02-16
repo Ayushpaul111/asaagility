0fcf747a54157d9cad287b05236c34243b723079
ayushpaul111 <ayushpaul1111@gmail.com>
2026-02-16 17:00:49 +0530

    feat: configure image remote patterns in Next.js config for enhanced image handling

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "img.freepik.com",
+      },
+      {
+        protocol: "https",
+        hostname: "hoirqrkdgbmvpwutwuwj-all.supabase.co",
+      },
+    ],
+  },
+};
 
 module.exports = nextConfig;
 
